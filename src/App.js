dedb63bccaab1a5eba7a8207fed2bc5b504ea91f
yrohan@780 <rohan@triosoft.in>
2024-01-12 17:32:30 +0530

    sdsdsdfd

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import "./App.css";
 import MyForm from "./Form";
 
 function App() {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js", { scope: "/public/" })
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/service-worker.js", { scope: "/public/" })
+  //       .then((registration) => {
+  //         console.log(
+  //           "Service Worker registered with scope:",
+  //           registration.scope
+  //         );
+  //       })
+  //       .catch((error) => {
+  //         console.error("Service Worker registration failed:", error);
+  //       });
+  //   }
+  // }, []);
 
   return (
     <div className="App">
