c518e62954babb4103f1040bc1949ac762c79941
yrohan@780 <rohan@triosoft.in>
2024-01-13 18:03:01 +0530

    dfgfdg

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
       // List of resources to cache
-      const resourcesToCache = [offlineUrl];
+      const resourcesToCache = [offlineUrls];
 
       // Use Promise.all to handle multiple add operations
       return Promise.all(
